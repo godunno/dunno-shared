1882ef9bcece4b3c74eaf7add3d4e4c6e7432293
Fagner Moura <fagnermoura@gmail.com>
2014-04-09 18:58:32 -0300

    release poll

--- dunno-shared/Models/DUNPoll.h
+++ dunno-shared/Models/DUNPoll.h
@@ -1,9 +1,17 @@
 #import <Mantle/Mantle.h>
 
+typedef NS_ENUM(NSInteger, DUNPollStatus) {
+  DUNPollAvailable,
+  DUNPollReleased,
+};
+
+
 @interface DUNPoll : MTLModel<MTLJSONSerializing>
 
 @property (nonatomic, readonly, copy) NSString *uuid;
 @property (nonatomic, readonly, copy) NSString *content;
+@property (nonatomic, assign) DUNPollStatus status;
+@property (nonatomic, readonly, strong) NSDate *releaseddAt;
 
 @property (nonatomic, readonly, strong) NSArray *options;
 
--- dunno-shared/Models/DUNPoll.m
+++ dunno-shared/Models/DUNPoll.m
@@ -1,18 +1,38 @@
 #import "DUNPoll.h"
 #import "DUNPollOption.h"
 
+#import <ISO8601DateFormatter/ISO8601DateFormatter.h>
+
 @implementation DUNPoll
 
 #pragma  mark -
 #pragma  mark - Mantle JSON Serializer
 
-+ (NSDictionary *)JSONKeyPathsByPropertyKey
-{
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
   return  @{};
 }
 
 + (NSValueTransformer *)optionsJSONTransformer {
   return [NSValueTransformer mtl_JSONArrayTransformerWithModelClass:DUNPollOption.class];
 }
+
++ (NSValueTransformer *)statusJSONTransformer
+{
+  return [MTLValueTransformer transformerWithBlock:^id(id inObj) {
+    if([(NSString*)inObj isEqualToString:@"released"])
+      return @(DUNPollReleased);
+    else
+      return @(DUNPollAvailable);
+  }];
+}
+
++ (NSValueTransformer *)releasedAtJSONTransformer {
+  ISO8601DateFormatter *formatter = [[ISO8601DateFormatter alloc] init];
+  
+  return [MTLValueTransformer transformerWithBlock:^id(id str) {
+    return [formatter dateFromString:str];
+  }];
+}
+
 
 @end
