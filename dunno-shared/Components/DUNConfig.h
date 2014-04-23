888280a1ce447a6429eb629ee434ae53af44c9e3
Fagner Moura <fagnermoura@gmail.com>
2014-04-23 16:43:33 -0300

    change config header

--- dunno-shared/Components/DUNConfig.h
+++ dunno-shared/Components/DUNConfig.h
@@ -1,5 +1,4 @@
-// dunno heroku - development
-//#define PUSHER_ACCESS_KEY @"eb2e8026ff09c08e1081"
-
-//personal account
 #define PUSHER_ACCESS_KEY @"e7433e907103b33d7298"
+
+//heroku
+//#define PUSHER_ACCESS_KEY @"eb2e8026ff09c08e1081"
