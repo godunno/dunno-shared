748f43bae1fcbb3601610c3aa5d99816424d81fb
Fagner Moura <fagnermoura@gmail.com>
2014-04-23 00:50:12 -0300

    change script to try to build and run all projects tests

--- xcspec.sh
+++ xcspec.sh
@@ -4,5 +4,29 @@ if ! type "xctool" > /dev/null; then
   exit 1;
 fi
 
-current_directory=${PWD##*/}
-xctool -workspace `pwd`/${current_directory}.xcworkspace -scheme ${current_directory} test -sdk iphonesimulator 
+
+PARENT_DIR="$(dirname "${PWD}")"
+
+function build_and_test()
+{
+  local xcworkspace_file="${PARENT_DIR}/$1/$2.xcworkspace"
+
+  if ( [[ -d "$xcworkspace_file" ]] ) 
+  then 
+    echo "==================="
+    echo "xBuild and xTest -> $1..."
+    echo "==================="
+    xctool -workspace $xcworkspace_file scheme $2 test -sdk iphonesimulator 
+  else
+    echo "==================="
+    echo "File not found (${xcworkspace_file})"
+    echo "Projects should be at the same path"
+    echo "==================="
+  fi
+}
+
+echo "Use projects path: ${PARENT_DIR}"
+
+build_and_test "dunno-shared" "dunno-shared"
+build_and_test "dunno-ios" "dunno"
+build_and_test "dunno-ipad" "dunno-ipad"
