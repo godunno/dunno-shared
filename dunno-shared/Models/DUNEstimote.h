43d27519caeb0925d6f64db986e8e12e4940f2b5
Fagner Moura <fagnermoura@gmail.com>
2014-04-29 16:23:35 -0300

    add title to estimote

--- dunno-shared/Models/DUNEstimote.h
+++ dunno-shared/Models/DUNEstimote.h
@@ -3,6 +3,7 @@
 @interface DUNEstimote : MTLModel
 
 @property (nonatomic, readonly, copy) NSString *entityId;
+@property (nonatomic, readonly, copy) NSString *title;
 @property (nonatomic, readonly, copy) NSString *proximityUUID;
 @property (nonatomic, readonly, strong) NSNumber *majorIdentifier;
 @property (nonatomic, readonly, strong) NSNumber *minorIdentifier;
